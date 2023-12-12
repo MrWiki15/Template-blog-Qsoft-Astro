ae6689e5ca5ae3f9310e1c6c4c5c39fe59abaab2
MrWiki <wikicanton439@gmail.com>
2023-12-12 02:37:22 -0500

    Dando el responsive a Android

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'WEB3-NFT.XYZ';
+export const SITE_TITLE = 'BLOCKCHAIN';
 export const SITE_DESCRIPTION = 'Bienvenido al mejor Blog Crypto';
